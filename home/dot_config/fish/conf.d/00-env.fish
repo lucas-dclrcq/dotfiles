4d1c3a46d216d6b974a19533cf27ab4d5d91a9b8
Lucas Declercq <lucas.declercq@hoohoot.org>
2025-05-29 20:08:01 +0200

    fix: try on mac

--- home/dot_config/fish/conf.d/00-env.fish
+++ home/dot_config/fish/conf.d/00-env.fish
@@ -5,4 +5,7 @@ set -gx VISUAL vim
 set -gx EDITOR vim
 set -gx KUBE_EDITOR vim
 
+set -gx GOPATH $HOME/go
+
 fish_add_path --global --prepend $HOME/.scripts
+fish_add_path --global --prepend $HOME/go/bin
